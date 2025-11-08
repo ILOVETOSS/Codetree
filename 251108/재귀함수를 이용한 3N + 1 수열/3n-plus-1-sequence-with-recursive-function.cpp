77395b151d8eccd152fe1e9a376c1a83cbb2cfdf
토스가고싶어요 <117723053+ILOVETOSS@users.noreply.github.com>
2025-11-08 21:37:36 +0900

    Title: 재귀함수를 이용한 3N + 1 수열, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 251108/재귀함수를 이용한 3N + 1 수열/3n-plus-1-sequence-with-recursive-function.cpp
+++ 251108/재귀함수를 이용한 3N + 1 수열/3n-plus-1-sequence-with-recursive-function.cpp
@@ -2,23 +2,23 @@
 
 using namespace std;
 
-int n;
-int f(int x){
-    if(x == 1){
+// a가 3n + 1 수열을 총 몇번 반복해야 1이 되는지 반환합니다.
+int CountNumber(int a) {
+    if(a == 1)
         return 0;
-    }
-    if(x % 2 == 0){
-        return (x / 2) + 1;
-    }
-    else{
-        return (3 * x + 1) + 1;
-    }
+
+    if(a % 2 == 0)
+        return CountNumber(a / 2) + 1;
+    else
+        return CountNumber(3 * a + 1) + 1;
 }
 
 int main() {
+    // 변수 선언 및 입력:
+    int n;
     cin >> n;
-    cout << f(n);
-    // Please write your code here.
+
+    cout << CountNumber(n);
 
     return 0;
 }
