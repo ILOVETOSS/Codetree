60f0166174f757115b976e4ba554d4aa5ed5e127
토스가고싶어요 <117723053+ILOVETOSS@users.noreply.github.com>
2025-11-09 18:37:36 +0900

    Title: 중앙값 계산 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251109/중앙값 계산 2/get-median-2.cpp
+++ 251109/중앙값 계산 2/get-median-2.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
 #include <algorithm>
+
+#define MAX_N 100
+
 using namespace std;
 
+// 변수 선언
 int n;
-int arr[100];
+int arr[MAX_N];
 
 int main() {
+    // 입력:
     cin >> n;
-    for (int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++)
         cin >> arr[i];
-        sort(arr, arr + i + 1); // 지금까지 입력된 수 정렬
-
-        if (i % 2 == 0) { // i가 짝수 = 입력된 개수가 홀수일 때
+    
+    // 홀수 번째 수를 지날때마다 정렬을 진행한 후 가운데 값을 출력합니다.
+    for(int i = 0; i < n; i++) {
+        if(i % 2 == 0) {
+            // 오름차순 정렬을 진행합니다.
+            sort(arr, arr + i + 1);
+            // 가운데 값을 출력합니다.
             cout << arr[i / 2] << " ";
         }
     }
