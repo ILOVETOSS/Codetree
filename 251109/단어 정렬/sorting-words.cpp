6e9b0d198949b1fd0ba39b81894de47c57a1f381
토스가고싶어요 <117723053+ILOVETOSS@users.noreply.github.com>
2025-11-09 16:49:24 +0900

    Title: 단어 정렬, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251109/단어 정렬/sorting-words.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+
+using namespace std;
+
+int n;
+string word[100];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> word[i];
+    }
+    sort(word, word+n);
+    for(int j = 0; j < n; j++){
+        cout << word[j] << "\n";
+    }
+
+    // Please write your code here.
+
+    return 0;
+}
