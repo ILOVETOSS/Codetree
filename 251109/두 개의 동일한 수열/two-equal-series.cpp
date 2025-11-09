905f3d4d9a3789794f96ccaac6da1367a9b6b0f5
토스가고싶어요 <117723053+ILOVETOSS@users.noreply.github.com>
2025-11-09 17:11:08 +0900

    Title: 두 개의 동일한 수열, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251109/두 개의 동일한 수열/two-equal-series.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+int A[100];
+int B[100];
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+    sort(A, A+n);
+
+
+    for (int i = 0; i < n; i++) {
+        cin >> B[i];
+    }
+    sort(B, B+n);
+
+    for(int i = 0; i < n; i++){
+        if(A[i] == B[i]){
+            cout << "Yes";
+        }
+        else{
+            cout << "No";
+        }
+    }
+
+    // Please write your code here.
+
+    return 0;
+}
