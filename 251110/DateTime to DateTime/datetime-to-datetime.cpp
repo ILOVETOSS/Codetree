f7ae56c99e9c7b5731d950bd9514880a7d1cd209
토스가고싶어요 <117723053+ILOVETOSS@users.noreply.github.com>
2025-11-10 18:54:07 +0900

    Title: DateTime to DateTime, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251110/DateTime to DateTime/datetime-to-datetime.cpp
+++ 251110/DateTime to DateTime/datetime-to-datetime.cpp
@@ -1,32 +1,27 @@
 #include <iostream>
-
 using namespace std;
 
-int a, b, c;
-
-
-
 int main() {
-    int y,m,d,h,mm;
-    y = 2011;
-    m = 11;
-
-    d = 11;
-    h = 11;
-    mm = 11;
-    int s_day;
-    int s_h;
-    int cnt;
-    int s_m;
-
+    int a, b, c;
     cin >> a >> b >> c;
-    s_day = a - d;
-    s_h = b - h;
-    s_m = c - mm;
-    cnt += s_m + 60 * s_h + 60 * (s_day * 24);
+
+    int base_day = 11, base_hour = 11, base_min = 11;
+    int cnt = 0;
+
+    // 입력이 기준보다 빠른 경우
+    if (a < base_day || 
+       (a == base_day && b < base_hour) || 
+       (a == base_day && b == base_hour && c < base_min)) {
+        cout << -1;
+        return 0;
+    }
+
+    int s_day = a - base_day;
+    int s_hour = b - base_hour;
+    int s_min = c - base_min;
+
+    cnt = s_min + 60 * s_hour + 60 * (s_day * 24);
+
     cout << cnt;
-
-    // Please write your code here.
-
     return 0;
 }
