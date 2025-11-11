73805f50e9132ba046013e6bb13441ecc3ba660f
토스가고싶어요 <117723053+ILOVETOSS@users.noreply.github.com>
2025-11-11 19:10:27 +0900

    Title: 최대로 겹치는 구간, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251111/최대로 겹치는 구간/maximum-overlapped-segments.cpp
+++ 251111/최대로 겹치는 구간/maximum-overlapped-segments.cpp
@@ -1,27 +1,41 @@
 #include <iostream>
 
+#define MAX_N 100
+#define MAX_R 200
+#define OFFSET 100
+
 using namespace std;
 
 int n;
-int x1[100], x2[100];
+int x1[MAX_N], x2[MAX_N];
 
-int arr[1001] = {};
+int checked[MAX_R + 1];
+
 int main() {
+    // 입력
     cin >> n;
-
-    for (int i = 0; i < n; i++) {
+    
+    for(int i = 0; i < n; i++) {
         cin >> x1[i] >> x2[i];
+        
+        // OFFSET을 더해줍니다.
+        x1[i] += OFFSET;
+        x2[i] += OFFSET;
     }
-    for(int i = 0; i < n; i++){
-        for(int j = x1[i]; j < x2[i]; j++){
-            arr[j]++;
-        }
-    }
-    int mx = 0;
-    for(int i = 0; i <= 100; i++){
-        mx = max(mx, arr[i]);
-    }
-    // Please write your code here.
-    cout << mx;
+    
+    // 구간을 칠해줍니다.
+    // 구간 단위로 진행하는 문제이므로
+    // x2[i]에 등호가 들어가지 않음에 유의합니다.
+    for(int i = 0; i < n; i++)
+        for(int j = x1[i]; j < x2[i]; j++)
+            checked[j]++;
+    
+    // 최댓값을 구합니다.
+    int max = 0;
+    for(int i = 0; i <= MAX_R; i++)
+        if(checked[i] > max)
+            max = checked[i];
+    
+    cout << max;
     return 0;
 }
