2c86302eb3793889a00c4601c2e0ae29f4c249ea
토스가고싶어요 <117723053+ILOVETOSS@users.noreply.github.com>
2025-11-11 09:53:17 +0900

    Title: 여러가지 진수변환, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251111/여러가지 진수변환/various-numeral-system-transformations.cpp
+++ 251111/여러가지 진수변환/various-numeral-system-transformations.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
+
 using namespace std;
 
+int N, B;
+int arr[100] = {};
+int cnt;
 int main() {
-    int N, B;
-    int arr[100];
-    int cnt = 0;
-
     cin >> N >> B;
 
-    // 진법 변환 과정
-    while (N > 0) {
+    while(true){
+        if(N < B){
+            arr[cnt++] =  N;
+            break;
+        }
+
         arr[cnt++] = N % B;
         N /= B;
     }
 
-    // 결과를 거꾸로 출력
-    for (int i = cnt - 1; i >= 0; i--) {
-        if (arr[i] >= 10)
-            cout << char('A' + (arr[i] - 10)); // 예: 10 → A, 11 → B ...
-        else
-            cout << arr[i];
+    
+    for(int i = cnt - 1; i >= 0; i--){
+        cout << arr[i];
     }
 
     return 0;
