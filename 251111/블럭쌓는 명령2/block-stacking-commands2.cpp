5940b0f366105d3634c0b5c60d47ff5b7fb6b656
토스가고싶어요 <117723053+ILOVETOSS@users.noreply.github.com>
2025-11-11 18:42:38 +0900

    Title: 블럭쌓는 명령2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251111/블럭쌓는 명령2/block-stacking-commands2.cpp
+++ 251111/블럭쌓는 명령2/block-stacking-commands2.cpp
@@ -5,20 +5,21 @@ using namespace std;
 
 int x1[100], x2[100];
 int arr[100] = {};
+int a,b;
 int main() {
     cin >> a >> b;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i <= b; i++) {
         cin >> x1[i] >> x2[i];
     }
-    for(int i = 1; i <= x2; i++){
+    for(int i = 1; i <= b; i++){
         for(int j = x1[i]; j <= x2[i]; j++){
-            arr[j];
+            arr[j]++;
         }
     }
     int mx = 0;
-    for(int i = 1; i <= x2; i++){
-        mx = max(i,arr[i]);
+    for(int i = 1; i <= b; i++){
+        mx = max(mx,arr[i]);
     }
     cout << mx;
     // Please write your code here.
