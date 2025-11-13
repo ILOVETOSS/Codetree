5640a578e71b40bcb39bdef8b4a7f4e2ace13537
토스가고싶어요 <117723053+ILOVETOSS@users.noreply.github.com>
2025-11-13 17:43:41 +0900

    Title: 수열 조작, Time: 9ms, Memory: 1MB, Status: Passed - Codetree

--- 251113/수열 조작/sequence-manipulation.cpp
+++ 251113/수열 조작/sequence-manipulation.cpp
@@ -2,21 +2,25 @@
 #include <deque>
 using namespace std;
 
-int N;
-deque<int> s;
 int main() {
+    int N;
     cin >> N;
-    for(int i = 0; i <= N; i++){
-        s.push(i);
-    }
-    for(int i = 0; i <= N; i++){
-        s.pop_front();
-        int a
-        s.front(a);
-        s.pop_front();
-        s.push_back(a);
-    }
-    cout << s.front();
 
+    deque<int> s;
+
+    // 1 ~ N까지 카드 넣기
+    for(int i = 1; i <= N; i++){
+        s.push_back(i);
+    }
+
+    // 카드가 1장 남을 때까지 반복
+    while(s.size() > 1) {
+        s.pop_front();           // 맨 위 카드 버리기
+        int a = s.front();       // 다음 카드 맨 앞 보기
+        s.pop_front();           // 그 카드 빼기
+        s.push_back(a);          // 맨 뒤로 보내기
+    }
+
+    cout << s.front();           // 마지막 카드 출력
     return 0;
 }
