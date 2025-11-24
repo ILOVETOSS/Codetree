9112b3144dfd58f9cbdd3d8ef11f992f053be0ad
토스가고싶어요 <117723053+ILOVETOSS@users.noreply.github.com>
2025-11-24 18:50:36 +0900

    Title: 별표 출력하기 5, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251124/별표 출력하기 5/print-star-5.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+
+    int n;
+    cin >> n;
+    for(int i = n; i >= 1; i--){
+        for(int j = 1; j <= i; j++){
+            for(int k = 1; k <= i; k++){
+                cout << "*";
+            }
+            cout << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
